19ea334176173b7749c8f720461d695f2b420bf9
Danara <chaykalak.d08@gmail.com>
2025-09-20 13:19:54 +0300

    sdjfn sdb

--- src/Loop.kt
+++ src/Loop.kt
@@ -1,12 +1,28 @@
 fun main() {
-    println("Начинаем ритуал защиты!")
-    var index = 0
-    repeat(times = 5) { index ->
-        if (index == 2) {
-            println("Пропускаем шаг ${index+1} из-за магического вмешательства!")
-            return@repeat
-        }
-        println("Заклинание защиты активировано! (Шаг ${index +1})")
-    }
-    println("Замок защищен, DUDE!")
+//    println("Начинаем ритуал защиты!")
+//    var index = 0
+//    repeat(times = 5) { index ->
+//        if (index == 2) {
+//            println("Пропускаем шаг ${index+1} из-за магического вмешательства!")
+//            return@repeat
+//        }
+//        println("Заклинание защиты активировано! (Шаг ${index +1})")
+//    }
+//    println("Замок защищен, DUDE!")
+    //задание 8
+//    val months = listOf("Python","Kotlin","C#","C++","Java")
+//    println(months[0])
+//    println(months.size)
+//    println("Kotlin" in months)
+//val daysInMonth = listOf(31,28,31,30,31,30,31,31,30,31,30,31)
+//    print("Введите номер месяца (1-12): ")
+//    val input = readln().toInt()
+//    if (input in 1..12) {
+//        val days = daysInMonth[input - 1]
+//        println("В этом месяце $days дней.")
+//    } else {
+//        println("Некорректный номер месяца.")
+//    }
+    // задание 9
+
 }
--- src/Range.kt
+++ src/Range.kt
@@ -1,13 +1,17 @@
 fun main(){
     val numbers=1 ..100
-    val symbol= 'a'..'z'
-    print("Введите ваш уровень: ")
-    val level = readln().toInt()
-    if (level in 1..50){
-        println("Уровень $level соответствует треюовниям1 Вы можете зайти в данж!")
+    val symbol= 'z'..'a'
+    val w = 5..11
+    val a = 10.. 50 step 3
 
-    }
-    else {
-        println("высокий уровень! вход запрещен" )
-    }
+//    print("Введите ваш уровень: ")
+//    val level = readln().toInt()
+//    if (level in 1..50){
+//        println("Уровень $level соответствует треюовниям1 Вы можете зайти в данж!")
+//
+//    }
+//    else {
+//        println("высокий уровень! вход запрещен" )
+//    }
+
 }
