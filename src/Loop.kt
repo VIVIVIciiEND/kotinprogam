99468a3952c08f245a517c07bff3b9e178f2022f
viviviciiend <akirmandinara@gmail.com>
2025-10-15 15:58:18 +0300

    fff

--- src/Loop.kt
+++ src/Loop.kt
@@ -24,5 +24,21 @@ fun main() {
 //        println("Некорректный номер месяца.")
 //    }
     // задание 9
-
+//    val lowerCaseLetter : Char = 'a'
+//    val upperCaseLatter : Char = 'Q'
+//    val number: Char ='1'
+//    val space: Char = ' '
+//    val dollar : Char = '$'
+//    val ch ='\u0040'
+//    val ch ='a'
+//    println(ch.code)
+//    val num = 97
+//    println(num.toInt())
+//    val ch : Char =readln().first()
+//    val ch1= 'b'
+//    val ch2 = ch1 + 1
+//    val ch3 = ch2 -2
+//    val ch = 'a'
+//    val ch1 = 1+ ch
+//    val charsSum = 'a'+ 'b'
 }
